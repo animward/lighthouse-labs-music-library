6a73067b8315861b90eb2e9786c87fe7a990b784
animward <142351228+animward@users.noreply.github.com>
2024-03-09 01:31:07 +0000

    printPlaylist

--- library-methods.js
+++ library-methods.js
@@ -7,7 +7,10 @@ const library = {
   
     // it's now a method!
     printPlaylists: function () {
-      // ...
+        for (const playlistId in this.playlists) {
+            const playlist = this.playlists[playlistId];
+            console.log(`${playlist.id}: ${playlist.name} - ${playlist.tracks.length} track${playlist.tracks.length !== 1 ? 's' : ''}`);
+          }
     },
   
     // it's now a method!
